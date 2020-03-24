4841417b3312c9c4fa1ea97f928724080c4f0439
Patwn <yanran170214@outlook.com>
2020-03-24 21:17:20 +0800

    manager项目搭建

---
+++ manager.sql
@@ -0,0 +1,242 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.7.23 : Database - manager
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`manager` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `manager`;
+
+/*Table structure for table `sys_config` */
+
+DROP TABLE IF EXISTS `sys_config`;
+
+CREATE TABLE `sys_config` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `value` varchar(100) DEFAULT 'null' COMMENT '数据值',
+  `label` varchar(100) DEFAULT 'null' COMMENT '标签名',
+  `type` varchar(100) DEFAULT 'null' COMMENT '类型',
+  `description` varchar(100) NOT NULL COMMENT '描述',
+  `sort` decimal(10,0) NOT NULL COMMENT '排序(升序)',
+  `create_by` varchar(50) DEFAULT 'null' COMMENT '创建人',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `last_update_by` varchar(50) DEFAULT 'null' COMMENT '更新人',
+  `last_update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  `remarks` varchar(255) DEFAULT 'null' COMMENT '备注信息',
+  `del_flag` tinyint(4) DEFAULT '0' COMMENT '是否删除  -1:删除  0:删除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC COMMENT='系统配置表';
+
+/*Data for the table `sys_config` */
+
+/*Table structure for table `sys_dept` */
+
+DROP TABLE IF EXISTS `sys_dept`;
+
+CREATE TABLE `sys_dept` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `name` varchar(50) DEFAULT 'null' COMMENT '机构名称',
+  `parent_id` bigint(20) DEFAULT NULL COMMENT '上级机构id,一级机构为0',
+  `order_num` int(11) DEFAULT '0' COMMENT '排序',
+  `create_by` varchar(50) DEFAULT 'null' COMMENT '创建人',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `last_update_by` varchar(50) DEFAULT 'null' COMMENT '更新人',
+  `last_update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  `del_flag` tinyint(4) DEFAULT '0' COMMENT '是否删除   -1:已删除   0:正常',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC COMMENT='机构管理';
+
+/*Data for the table `sys_dept` */
+
+/*Table structure for table `sys_dict` */
+
+DROP TABLE IF EXISTS `sys_dict`;
+
+CREATE TABLE `sys_dict` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `value` varchar(100) NOT NULL COMMENT '数据值',
+  `label` varchar(100) NOT NULL COMMENT '标签名',
+  `type` varchar(100) NOT NULL COMMENT '类型',
+  `description` varchar(100) NOT NULL COMMENT '描述',
+  `sort` decimal(10,0) NOT NULL COMMENT '排序(升序)',
+  `create_by` varchar(50) DEFAULT 'null' COMMENT '创建人',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `last_update_by` varchar(50) DEFAULT 'null' COMMENT '更新人',
+  `last_update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  `remarks` varchar(255) DEFAULT 'null' COMMENT '备注信息',
+  `del_flag` tinyint(4) DEFAULT '0' COMMENT '是否删除  -1:删除  0:正常',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC COMMENT='字典表';
+
+/*Data for the table `sys_dict` */
+
+/*Table structure for table `sys_log` */
+
+DROP TABLE IF EXISTS `sys_log`;
+
+CREATE TABLE `sys_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `user_name` varchar(50) DEFAULT 'null' COMMENT '用户名',
+  `operation` varchar(50) DEFAULT 'null' COMMENT '用户操作',
+  `method` varchar(200) DEFAULT 'null' COMMENT '请求方法',
+  `params` varchar(5000) DEFAULT 'null' COMMENT '请求参数',
+  `time` bigint(20) NOT NULL COMMENT '执行时长(毫秒)',
+  `ip` varchar(64) DEFAULT 'null' COMMENT 'ip地址',
+  `create_by` varchar(50) DEFAULT 'null' COMMENT '创建人',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `last_update_by` varchar(50) DEFAULT 'null' COMMENT '更新人',
+  `last_update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC COMMENT='系统操作日志';
+
+/*Data for the table `sys_log` */
+
+/*Table structure for table `sys_login_log` */
+
+DROP TABLE IF EXISTS `sys_login_log`;
+
+CREATE TABLE `sys_login_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `user_name` varchar(50) DEFAULT 'null' COMMENT '用户名',
+  `status` varchar(50) DEFAULT 'null' COMMENT '登录状态   online在线  login',
+  `ip` varchar(64) DEFAULT 'null' COMMENT 'ip地址',
+  `create_by` varchar(50) DEFAULT 'null' COMMENT '创建人',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `last_update_by` varchar(50) DEFAULT 'null' COMMENT '更新人',
+  `last_update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC COMMENT='系统登录日志';
+
+/*Data for the table `sys_login_log` */
+
+/*Table structure for table `sys_menu` */
+
+DROP TABLE IF EXISTS `sys_menu`;
+
+CREATE TABLE `sys_menu` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `name` varchar(50) DEFAULT 'null' COMMENT '菜单名称',
+  `parent_id` bigint(20) DEFAULT '0' COMMENT '父菜单id,一级菜单为0',
+  `url` varchar(200) DEFAULT 'null' COMMENT '菜单url,类型:1.普通页面(如用户管理,/sys/usr)2.嵌套完整外部页面,以http(s)开头的连接3.嵌套服务器页面,使用iframe:前缀+目标url',
+  `perms` varchar(500) DEFAULT 'null' COMMENT '授权(多个用逗号分隔,如   sys:user:add',
+  `type` int(11) DEFAULT '0' COMMENT '类型   0:目录  1:菜单  2:按钮',
+  `icon` varchar(50) DEFAULT 'null' COMMENT '菜单图标',
+  `order_num` int(11) DEFAULT '0' COMMENT '排序',
+  `create_by` varchar(50) DEFAULT 'null' COMMENT '创建人',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `last_update_by` varchar(50) DEFAULT 'null' COMMENT '更新人',
+  `last_update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  `del_flag` tinyint(4) DEFAULT '0' COMMENT '是否删除  -1:已删除  0:正常',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC COMMENT='菜单管理';
+
+/*Data for the table `sys_menu` */
+
+/*Table structure for table `sys_role` */
+
+DROP TABLE IF EXISTS `sys_role`;
+
+CREATE TABLE `sys_role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `name` varchar(100) DEFAULT 'null' COMMENT '角色名称',
+  `remark` varchar(100) DEFAULT 'null' COMMENT '备注',
+  `create_by` varchar(50) DEFAULT 'null' COMMENT '创建人',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `last_update_by` varchar(50) DEFAULT 'null' COMMENT '更新人',
+  `last_update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  `del_flag` tinyint(4) DEFAULT '0' COMMENT '是否删除  -1:已删除  0:正常',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC COMMENT='角色管理';
+
+/*Data for the table `sys_role` */
+
+/*Table structure for table `sys_role_dept` */
+
+DROP TABLE IF EXISTS `sys_role_dept`;
+
+CREATE TABLE `sys_role_dept` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `role_id` bigint(20) DEFAULT NULL COMMENT '角色id',
+  `dept_id` bigint(20) DEFAULT NULL COMMENT '机构id',
+  `create_by` varchar(50) DEFAULT 'null' COMMENT '创建人',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `last_update_by` varchar(50) DEFAULT 'null' COMMENT '更新人',
+  `last_update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC COMMENT='角色机构';
+
+/*Data for the table `sys_role_dept` */
+
+/*Table structure for table `sys_role_menu` */
+
+DROP TABLE IF EXISTS `sys_role_menu`;
+
+CREATE TABLE `sys_role_menu` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `role_id` bigint(20) DEFAULT '0' COMMENT '角色id',
+  `menu_id` bigint(20) DEFAULT NULL COMMENT '菜单id',
+  `create_by` varchar(50) DEFAULT 'null' COMMENT '创建人',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `last_update_by` varchar(50) DEFAULT 'null' COMMENT '更新人',
+  `last_update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC COMMENT='角色菜单';
+
+/*Data for the table `sys_role_menu` */
+
+/*Table structure for table `sys_user` */
+
+DROP TABLE IF EXISTS `sys_user`;
+
+CREATE TABLE `sys_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `name` varchar(50) NOT NULL COMMENT '用户名',
+  `nick_name` varchar(150) DEFAULT NULL COMMENT '昵称',
+  `avatar` varchar(150) DEFAULT NULL COMMENT '头像',
+  `password` varchar(100) DEFAULT NULL COMMENT '密码',
+  `salt` varchar(40) DEFAULT NULL COMMENT '加密盐',
+  `email` varchar(100) DEFAULT NULL COMMENT '邮箱',
+  `mobile` varchar(100) DEFAULT NULL COMMENT '手机号',
+  `status` tinyint(4) DEFAULT NULL COMMENT '状态  0:禁用  1:正常',
+  `dept_id` bigint(20) DEFAULT NULL COMMENT '机构id',
+  `create_by` varchar(50) DEFAULT NULL COMMENT '创建人',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `last_update_by` varchar(50) DEFAULT NULL COMMENT '更新人',
+  `del_flag` tinyint(4) DEFAULT '0' COMMENT '是否删除  -1:已删除  0:正常',
+  `last_update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8 COMMENT='用户管理';
+
+/*Data for the table `sys_user` */
+
+/*Table structure for table `sys_user_role` */
+
+DROP TABLE IF EXISTS `sys_user_role`;
+
+CREATE TABLE `sys_user_role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `user_id` bigint(20) DEFAULT '0' COMMENT '用户id',
+  `role_id` bigint(20) DEFAULT '0' COMMENT '角色id',
+  `create_by` varchar(50) DEFAULT 'null' COMMENT '创建人',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `last_update_by` varchar(50) DEFAULT 'null' COMMENT '更新人',
+  `last_update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC COMMENT='用户角色';
+
+/*Data for the table `sys_user_role` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
